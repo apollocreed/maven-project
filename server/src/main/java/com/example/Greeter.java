5cf16e7e0bb7eca0e0aacb9000a1c1c5f0214e0a
apollocreed <apollocreed@users.noreply.github.com>
2019-01-29 10:54:25 +0100

    javadoc

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,18 +1,18 @@
+/**
+* JAVADOC
+*
+*/
 package com.example;
-
-
-/*
+/**
 * Class java Greeter
 *
 */
 public class Greeter {
-
-/*
+/**
 * Constructeur
 *
 */
   public Greeter() {
-
   }
     public final String greet(final String someone) {
       return String.format("Hello Karl, %s!", someone);
