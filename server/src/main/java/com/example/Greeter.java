1dbe85fa3fe4e5ac26708e1bc0c428c6c416d54e
apollocreed <apollocreed@users.noreply.github.com>
2019-01-29 11:02:21 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,6 +14,10 @@ public class Greeter {
 */
   public Greeter() {
   }
+  /**
+  *@param final string
+  *@return string
+  */
     public final String greet(final String someone) {
       return String.format("Hello Karl, %s!", someone);
   }
