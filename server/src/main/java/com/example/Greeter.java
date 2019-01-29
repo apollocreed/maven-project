960aad485522eef6bbc49ec467442086dc739e71
apollocreed <apollocreed@users.noreply.github.com>
2019-01-29 10:51:48 +0100

    add comment

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,14 +1,20 @@
 package com.example;
 
 
+/*
+* Class java Greeter
+*
+*/
 public class Greeter {
 
-  
+  /*
+* Constructeur
+*
+*/
   public Greeter() {
 
   }
-
-  public final String greet(final String someone) {
-    return String.format("Hello Karl, %s!", someone);
+    public final String greet(final String someone) {
+      return String.format("Hello Karl, %s!", someone);
   }
 }
